1ae76121615e6d7a4a63b745d9f29bfdf95cf1af
Bissixp <fernandosbissi18@hotmail.com>
2022-03-10 17:54:18 -0300

    [Grupo 26]Requisito 3 feito

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CartButton from '../components/CartButton';
 
 class Home extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
         <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <CartButton />
       </div>
     );
   }
