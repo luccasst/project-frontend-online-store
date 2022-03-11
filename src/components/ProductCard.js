aa92187ef48c042734aa8a0582e3b0ff1f644e2c
Marcos Campos <marxmyers@icloud.com>
2022-03-11 15:47:33 -0300

    [Grupo 26] Requisito 05 Ok

---
+++ src/components/ProductCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends React.Component {
+  render() {
+    const { title, thumbnail, price } = this.props;
+    return (
+      <section data-testid="product">
+        <img src={ thumbnail } alt={ title } />
+        <p>{ price }</p>
+        <h3>{ title }</h3>
+      </section>
+
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default ProductCard;
